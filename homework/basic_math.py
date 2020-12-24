1ba2a91d820e6b9e063e826b785261312e4c44fd
= <=>
2020-12-24 23:32:27 +0900

    Add basic example for homework code : basic_math.get_greatest

--- homework/basic_math.py
+++ homework/basic_math.py
@@ -6,12 +6,25 @@
 여기서 간단한 수학을 하는 프로그램을 만들것입니다. 
 """
 
-"""
-1. 주어진 리스트에서 최고로 큰 숫자를 반환하세요.
-"""
-def get_greatest(list):
-    pass
+def get_greatest(number_list):
+    '''
+    주어진 리스트에서 가장 큰 숫자를 반환함
 
+        Parameters:
+            number_list (list): integer로 값으로만 구성된 list 
+            ex - [10, 33, 22, 99, 33]
+
+        Returns:
+            greatest_number (int): parameter number_list 중 가장 큰 값
+
+        Examples:
+            >>> number_list = [39, 54, 32, 11, 99]
+            >>> import basic_math as bm
+            >>> bm.get_greatest(number_list)
+            99
+    '''
+    greatest_number = None
+    return greatest_number
 
 """
 2. 주어진 리스트에서 제일 작은 숫자를 반환하세요.
