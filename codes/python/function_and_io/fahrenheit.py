4f1653acc42258ba214de49372a3705ebab53f11
= <=>
2020-12-30 13:46:35 +0900

    Update function and io codes

--- codes/python/function_and_io/fahrenheit.py
+++ codes/python/function_and_io/fahrenheit.py
@@ -2,7 +2,7 @@ print("본 프로그램은 섭씨 온도를 화씨 온도로 변환시켜주는 
 print("변환하고 싶은 온도를 입력해주세요 : ")
 
 cel = float(input())
-fah = ((9/5) * cel) + 32
+fah = ((9 / 5) * cel) + 32
 
 print("섭씨온도 : %.2f" % cel)
 print("섭씨온도 : %.2f" % fah)
